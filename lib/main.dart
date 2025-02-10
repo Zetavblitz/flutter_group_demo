b6097f0d2f981287d418375ca02bdaed4a23cd42
christianherpin <108888501+christianherpin@users.noreply.github.com>
2025-02-10 17:00:27 -0600

    Mark was here

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,10 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times: Ian Wuz Here',
+              'You have pushed the button this many times:',
+            ),
+            const Text(
+              'Mark was here!',
             ),
             Text(
               '$_counter',
